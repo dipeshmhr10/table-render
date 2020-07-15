d58524aa98392b64ff03a867937939163d46cfd6
dipeshmhr10 <dipeshmhr10@gmail.com>
2020-07-15 11:23:41 +0545

    eeee

---
+++ src/Datas.js
@@ -0,0 +1,22 @@
+
+    const Info = [
+        {
+          fname: "Pierce",
+          lname: "Ruce",
+          address: "USA",
+          phonenumber: "62000539544",
+          school: "George",
+        },
+        {
+          fname: "john",
+          lname: "Lucifer",
+          address: "Amsterdam",
+          phonenumber: "7853666251002202",
+          school: "Saint",
+        },
+      ];
+
+
+
+
+export default Info;
